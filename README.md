88bf67dc3f7ab86bfdb300e666a1bd992d26a280
Luca Critelli <lucacri@gmail.com>
2019-04-19 12:04:35 -0400

    Much better tagging. PHP 7.3

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # docker-laravel-alpine
 
 This docker image contains anything needed to run a Laravel web application.
+
+### Versions
+
+Versioning follows classic SEMVER. Anything under the `2` tag (`2`, `2.1`, `2.1.2`, etc) will have PHP 7.3
+
+| Tag | PHP Version |
+|-----|-------------|
+| `2`   | `7.3.x`       |
+| `1`   | `7.2.x`       |
--- root/etc/cont-init.d/29-optimize.sh
+++ root/etc/cont-init.d/29-optimize.sh
@@ -3,4 +3,8 @@
 if [ "$STARTUP_OPTIMIZE" -eq "1" ]; then
 php /var/www/artisan optimize >> /var/www/storage/logs/laravel.log 2>&1 || true
 echo "Done optimize"
+php /var/www/artisan view:cache >> /var/www/storage/logs/laravel.log 2>&1 || true
+echo "Done view cache"
+php /var/www/artisan event:cache >> /var/www/storage/logs/laravel.log 2>&1 || true
+echo "Done event cache"
 fi
