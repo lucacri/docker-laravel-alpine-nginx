b1cdee8b5a017b573fd965fa9d5748d750904c00
Luca Critelli <lucacri@gmail.com>
2019-05-30 12:58:16 -0400

    Use semver executable

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,9 +2,9 @@ TAG=$1
 
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
-IMAGE_TAG_PATCH=$(docker run --rm alpine/semver semver -c patch ${TAG})
-IMAGE_TAG_MINOR=$(docker run --rm alpine/semver semver -c minor ${TAG})
-IMAGE_TAG_MAJOR=$(docker run --rm alpine/semver semver -c major ${TAG})
+IMAGE_TAG_PATCH=$(/tmp/semver get patch ${TAG})
+IMAGE_TAG_MINOR=$(/tmp/semver get minor ${TAG})
+IMAGE_TAG_MAJOR=$(/tmp/semver get major ${TAG})
 
 docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest" && \
 docker tag "$IMAGE_NAME" "${IMAGE_NAME}:${IMAGE_TAG_PATCH}" && \
